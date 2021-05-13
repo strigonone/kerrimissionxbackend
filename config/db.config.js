9668d14f8073ec007718e589f6a8407e8b6abd0e
strigonone <s15jaychao@hotmail.com>
2021-05-13 16:31:04 +1200

    Small update to backend

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,16 @@
-const connection = mysql.createConnection({
-  host: "SG-MissionX-4265-mysql-master.servers.mongodirector.com",
-  database: "MissionXLibrary",
-  user: "sgroot",
-  password: "RKyjrf^MIB2sfR5z",
-  port: 3306,
-  ssl: {
-    ca: Buffer.from("../SSL/SLLCertificate.pem", "base64").toString("ascii"),
-  },
-}),
+const dbconfig = {
+	host: "SG-MissionX-4265-mysql-master.servers.mongodirector.com",
+	database: "MissionXLibrary",
+	user: "sgroot",
+	password: "RKyjrf^MIB2sfR5z",
+	port: 3306,
+	ssl: {
+		ca: Buffer.from(`../SSL/getClusterSSLPublicKey.pem`, "base64").toString(
+			"ascii"
+		),
 
-module.exports = { config };
+		rejectUnauthorized: false,
+	},
+};
+
+module.exports = { dbconfig };
